ac7ef97b9adabdcb2978a25cfe8fc6cc79a0dc36
feltfomo <241195017+feltfomo@users.noreply.github.com>
2025-11-02 10:46:15 -0800

    theme stuff

--- home.nix
+++ home.nix
@@ -1,19 +1,28 @@
 { pkgs, config, ... }:
-
 {
   home.username = "zynth";
   home.homeDirectory = "/home/zynth";
   home.stateVersion = "25.05";
 
-  xdg.configFile."kitty".source  = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/kitty";
-  xdg.configFile."rofi".source  = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/rofi";
-  xdg.configFile."hypr".source  = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/hypr";
-  xdg.configFile."waybar".source  = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/waybar";
+  home.packages = with pkgs; [
+    catppuccin-gtk
+    papirus-icon-theme
+    bibata-cursors
+  ];
+
+  home.sessionVariables = {
+    GTK_THEME = "Adwaita-dark";
+  };
+
+  xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/kitty";
+  xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/rofi";
+  xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/hypr";
+  xdg.configFile."waybar".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/waybar";
 
   gtk = {
     enable = true;
     theme = {
-      name = "Catppuccin-Mocha-Standard-Blue-Dark";
+      name = "Adwaita-dark";
       package = pkgs.catppuccin-gtk;
     };
     iconTheme = {
@@ -23,6 +32,21 @@
     cursorTheme = {
       name = "Bibata-Modern-Ice";
       package = pkgs.bibata-cursors;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "kvantum";
+    style = {
+      name = "kvantum";
+      package = pkgs.libsForQt5.qtstyleplugin-kvantum;
     };
   };
 }
--- packages.nix
+++ packages.nix
@@ -9,12 +9,20 @@
     btop
     fastfetch
     neovim
+    xfce.thunar
     git
     rofi-wayland
     waybar
     gsettings-desktop-schemas
     glib
     dconf-editor
+    gnome-themes-extra
+    libsForQt5.qt5ct
+    qt6ct
+    libsForQt5.qtstyleplugin-kvantum
+    qt6Packages.qtstyleplugin-kvantum
+    adwaita-qt
+    adwaita-qt6
     inputs.zen-browser.packages.${pkgs.system}.default
     inputs.swww.packages.${pkgs.system}.swww
   ];
