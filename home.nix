579b78a52093559589bc52cb03c97e41034768f0
feltfomo <241195017+feltfomo@users.noreply.github.com>
2025-11-03 10:55:12 -0800

    Add NPV Ambience + Beautiful Lyrics extensions

--- home.nix
+++ home.nix
@@ -1,5 +1,32 @@
-{ pkgs, config, ... }:
+{ pkgs, config, inputs, ... }:
+let
+  # Packaged things exported by spicetify-nix (extensions, apps, themes)
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+
+  # NPV Ambience (vendored from upstream repo)
+  npvAmbience = {
+    name = "npvAmbience.js";
+    src = (pkgs.fetchFromGitHub {
+      owner  = "ohitstom";
+      repo   = "spicetify-extensions";
+      rev    = "main";
+      sha256 = "sha256-x/xmOoSsr2zQ12ZpM4hDIs/ryvCusj8LShTLoH9KMq8=";
+    }) + "/npvAmbience";
+  };
+
+  # Local Beautiful Lyrics (from your flake)
+  beautifulLyrics = {
+    name = "beautiful-lyrics.mjs";
+    src  = builtins.path {
+      path = ./spice/beautiful-lyrics.mjs;  # relative path inside the flake
+      name = "beautiful-lyrics.mjs";
+    };
+  };
+in
 {
+  # Import spicetify-nix’s Home Manager module
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+
   home.username = "zynth";
   home.homeDirectory = "/home/zynth";
   home.stateVersion = "25.05";
@@ -14,9 +41,10 @@
     GTK_THEME = "Adwaita-dark";
   };
 
-  xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/kitty";
-  xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/rofi";
-  xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/hypr";
+  # Link configs
+  xdg.configFile."kitty".source  = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/kitty";
+  xdg.configFile."rofi".source   = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/rofi";
+  xdg.configFile."hypr".source   = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/hypr";
   xdg.configFile."waybar".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/waybar";
 
   gtk = {
@@ -33,18 +61,12 @@
       name = "Bibata-Modern-Ice";
       package = pkgs.bibata-cursors;
     };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
+    gtk3.extraConfig = { gtk-application-prefer-dark-theme = 1; };
+    gtk4.extraConfig = { gtk-application-prefer-dark-theme = 1; };
   };
 
   dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
+    "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
   };
 
   qt = {
@@ -54,5 +76,20 @@
       name = "kvantum";
       package = pkgs.libsForQt5.qtstyleplugin-kvantum;
     };
+  };
+
+  # Spicetify configuration
+  programs.spicetify = {
+    enable = true;
+
+    # Optional Marketplace page (browse-only)
+    enabledCustomApps = with spicePkgs.apps; [ marketplace ];
+
+    # Extensions: built-in + vendored + local
+    enabledExtensions =
+      (with spicePkgs.extensions; [ hidePodcasts ]) ++ [
+        npvAmbience
+        beautifulLyrics
+      ];
   };
 }
