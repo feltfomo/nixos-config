94f7aafd6c44547ddbfbe35f88958968934d22bd
feltfomo <241195017+feltfomo@users.noreply.github.com>
2025-11-11 14:29:04 -0800

    niri dot stuff

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -6,5 +6,6 @@
     "hypr".source   = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/hypr";
     "waybar".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/waybar";
     "nvim".source   = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/nvim";
+    "niri".source   = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dots/niri"
   };
 }
