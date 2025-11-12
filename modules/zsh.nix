4a91547311a7b00f6ff23855ca723f076da9a217
feltfomo <241195017+feltfomo@users.noreply.github.com>
2025-11-11 16:01:31 -0800

    added atuin to zsh and uhm probably gonna ditch niri

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -6,11 +6,22 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     
-    initContent = ''  # changed from initExtra
+    initContent = ''
       if [[ -z "$FASTFETCH_RAN" ]]; then
         export FASTFETCH_RAN=1
         fastfetch
       fi
     '';
+  };
+  
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
   };
 }
