4f7453cca2a1ca25b88d394bd3ab9c142f935f85
zynth <241195017+feltfomo@users.noreply.github.com>
2026-02-15 20:59:26 -0800

    added more options to zed config

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -7,10 +7,19 @@
       icon_theme = "Catppuccin Mocha";
       ui_font_size = 16;
       buffer_font_size = 15;
+      buffer_font_family = "JetBrains Mono";
       theme = {
         mode = "system";
         light = "One Light";
         dark = "Catppuccin Mocha";
+      };
+      vim_mode = true;
+      autosave = "on_focus_change";
+      format_on_save = "on";
+      tab_size = 2;
+      soft_wrap = "editor_width";
+      inlay_hints = {
+        enabled = true;
       };
     };
   };
