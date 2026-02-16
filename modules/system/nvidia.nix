7417865b98e8913446dd20d2d65e4629f65d6373
zynth <241195017+feltfomo@users.noreply.github.com>
2026-02-15 21:30:12 -0800

    cleaned up nvidia.nix

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 {
   services.xserver.videoDrivers = [ "nvidia" ];
-  
   hardware.nvidia = {
     modesetting.enable = true;
     open = false;
@@ -10,18 +9,12 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     nvidiaSettings = true;
   };
-
   boot.kernelParams = [
-    "nvidia_drm.modeset=1"
     "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
     "nvidia.NVreg_TemporaryFilePath=/var/tmp"
-    "nvme_load=YES"
     "nowatchdog"
   ];
-  
   boot.kernelModules = [ "nvidia-uvm" ];
-
-
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
